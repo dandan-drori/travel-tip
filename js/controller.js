fcb9457a9db3710ef52a7bb69a5028599d58c5f4
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-06-16 15:33:13 +0300

    merge starter project

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,67 @@
 'use strict'
+
+import { mapService } from './services/map.service.js'
+import { weatherService } from './services/weather-service.js'
+
+window.onload = onInit
+window.onAddMarker = onAddMarker
+window.onPanTo = onPanTo
+window.onGetLocs = onGetLocs
+window.onGetUserPos = onGetUserPos
+
+function onInit() {
+	mapService
+		.initMap()
+		.then(() => {
+			console.log('Map is ready')
+		})
+		.catch(() => console.log('Error: cannot init map'))
+}
+
+// This function provides a Promise API to the callback-based-api of getCurrentPosition
+function getPosition() {
+	console.log('Getting Pos')
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	})
+}
+
+function onAddMarker() {
+	console.log('Adding a marker')
+	mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
+}
+
+function onGetLocs() {
+	locService.getLocs().then(locs => {
+		console.log('Locations:', locs)
+		document.querySelector('.locs').innerText = JSON.stringify(locs)
+	})
+}
+
+function onGetUserPos() {
+	getPosition()
+		.then(pos => {
+			console.log('User position is:', pos.coords)
+			document.querySelector(
+				'.user-pos'
+			).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+		})
+		.catch(err => {
+			console.log('err!!!', err)
+		})
+}
+function onPanTo() {
+	console.log('Panning the Map')
+	mapService.panTo(35.6895, 139.6917)
+}
+
 import { weatherService } from './services/weather-service'
-import { mapService } from './js/service/map-service'
+import { mapService } from './service/map-service'
+
+function onGetWeather() {
+	weatherService.getWeather(31.11, 31.11).then(renderWeather)
+}
+
+function renderWeather(weather) {
+	console.log('weather', weather)
+}
